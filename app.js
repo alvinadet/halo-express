63f3f620bf0e58a99f46b3732cd44befd502126e
Muhammad Fari <fari.msenju@gmail.com>
2019-04-22 07:45:02 +0700

    add setup view engine and static file
    setup view engine and static file to render index on route "*"

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require("express");
 const router = require("./routes/router.js");
 const app = express();
 
+//static Files
+app.use(express.static("public"));
+
+//setup Views
+app.set("view engine", "ejs");
+
 //setup Routes
 app.use(router);
 
